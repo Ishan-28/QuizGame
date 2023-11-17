ec0a82b4d9f3e7a003bc4595268ce7bd1d67f195
Ishan Deshpande <deshpandeishan75@gmail.com>
2023-11-17 17:20:32 +0530

    Added new question sets using Trivia API

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
+from question_sets import problem_sets
 
 question_bank = []
-for question in question_data:
-    question_text = question["text"]
-    question_answer = question["answer"]
+for question in problem_sets:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
